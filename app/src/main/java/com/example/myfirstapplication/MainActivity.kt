dc93cbd01f481f1f4833b6c2e4fbfa24a261fe9e
Mohit Singh <mohitsingh@3fat.in>
2025-06-20 22:24:04 +0530

    navigation

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,13 +12,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.myfirstapplication.ui.theme.MyFirstApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,9 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppNavigator(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -39,53 +35,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    var clicks by remember { mutableStateOf(0) }
-    var color by remember { mutableStateOf(androidx.compose.ui.graphics.Color.Red) }
+fun AppNavigator(modifier: Modifier) {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "home", modifier = modifier.padding(20.dp)) {
+        composable("home") { Home(navController) }
+        composable("profile") { Profile(navController) }
+    }
+}
 
+@Composable
+fun Home(navController: NavController) {
     fun handleClick () {
-        clicks += 1
-        var test = clicks % 10
-        if (test < 5) {
-            color = androidx.compose.ui.graphics.Color.Red
-        } else {
-            color = androidx.compose.ui.graphics.Color.Green
-        }
+        navController.navigate("profile")
     }
 
-    fun handleReset () {
-        clicks = 0
-    }
-
-    Column (
-        modifier = Modifier.fillMaxSize().padding(24.dp)
-    ) {
-        Text(
-            text = "Button clicked $clicks times",
-            modifier = modifier
-        )
+    Column {
+        Text(text = "Welcome to home")
         Spacer(modifier = Modifier.padding(20.dp))
-        Button(
-            onClick = { handleClick() },
-            modifier = Modifier.background(color = color)
-        ) {
-            Text(text = "Click me")
-        }
-        if ( clicks > 0 ) {
-            Spacer(modifier = Modifier.padding(20.dp))
-            Button(
-                onClick = { handleReset() }
-            ) {
-                Text(text = "Reset")
-            }
+        Button(onClick = { handleClick() }) {
+            Text(text = "go to profile")
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyFirstApplicationTheme {
-        Greeting()
+fun Profile(navController: NavController) {
+    fun handleClick () {
+        navController.popBackStack()
+    }
+
+    Column {
+        Text(text = "Welcome to profile")
+        Spacer(modifier = Modifier.padding(20.dp))
+        Button(onClick = { handleClick() }) {
+            Text(text = "go to home")
+        }
     }
 }
